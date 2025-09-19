158ad82745e69a0be748d9ab100756833890721c
Mateus Rocha <tateusgameplay@gmail.com>
2025-09-19 15:47:22 -0300

    criando cenas

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,15 @@
+//using UnityEngine.SceneManagement;
+
+//void Update()
+    // {
+    //     Scene scene = SceneManager.GetActiveScene();
+    //     gameObject[] gos = GameObject.FindGameObjectsWithTag("Enemy");
+    //     print(gos.Length);
+    //     if (gos.Length == 0)
+    //     {
+    //         if (scene.name == "Level1")
+    //             SceneManager.LoadScene("Level2");
+    //         else if (scene.name == "Level2")
+    //             SceneManager.LoadScene("Level3");
+    //     }
+    // }
